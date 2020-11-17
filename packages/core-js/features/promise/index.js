82cc3af16089b9e9a36184f4b26ef1865a244088
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-18 06:19:47 +0700

    remove `esnext.aggregate-error` and `esnext.promise.any` (moved to stable es)

--- packages/core-js/features/promise/index.js
+++ packages/core-js/features/promise/index.js
@@ -1,6 +1,3 @@
 var parent = require('../../stable/promise');
-require('../../modules/esnext.aggregate-error');
-require('../../modules/esnext.promise.try');
-require('../../modules/esnext.promise.any');
 
 module.exports = parent;
