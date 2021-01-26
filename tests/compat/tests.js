65e493124bdd748a2c31d9d44505449d0e58ca3e
Denis Pushkarev <zloirock@zloirock.ru>
2021-01-27 01:31:12 +0700

    split `web.immediate` to `web.{ clear, set }-immediate`

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1493,6 +1493,9 @@ GLOBAL.tests = {
   'esnext.weak-set.of': function () {
     return WeakSet.of;
   },
+  'web.clear-immediate': function () {
+    return setImmediate && clearImmediate;
+  },
   'web.dom-collections.for-each': function () {
     return (!GLOBAL.NodeList || (NodeList.prototype.forEach && NodeList.prototype.forEach === [].forEach))
       && (!GLOBAL.DOMTokenList || (DOMTokenList.prototype.forEach && DOMTokenList.prototype.forEach === [].forEach));
@@ -1546,11 +1549,11 @@ GLOBAL.tests = {
     }
     return true;
   },
-  'web.immediate': function () {
-    return setImmediate && clearImmediate;
-  },
   'web.queue-microtask': function () {
     return Object.getOwnPropertyDescriptor(GLOBAL, 'queueMicrotask').value;
+  },
+  'web.set-immediate': function () {
+    return setImmediate && clearImmediate;
   },
   'web.set-interval': function () {
     return !MSIE9;
