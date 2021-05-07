4afcb068f696c53ed7e640f9ba800edd5edf9208
Denis Pushkarev <zloirock@zloirock.ru>
2021-05-07 22:21:25 +0300

    move `Symbol#@@primitive` to `es.symbol.to-primitive`

--- tests/tests/es.symbol.constructor.js
+++ tests/tests/es.symbol.constructor.js
@@ -42,7 +42,6 @@ QUnit.test('Well-known Symbols', assert => {
     'search',
     'species',
     'split',
-    'toPrimitive',
     'toStringTag',
     'unscopables',
   ];
@@ -54,12 +53,6 @@ QUnit.test('Well-known Symbols', assert => {
     assert.ok(!descriptor.writable, 'non-writable');
     assert.ok(!descriptor.configurable, 'non-configurable');
   }
-});
-
-QUnit.test('Symbol#@@toPrimitive', assert => {
-  const symbol = Symbol();
-  assert.isFunction(Symbol.prototype[Symbol.toPrimitive]);
-  assert.same(symbol, symbol[Symbol.toPrimitive](), 'works');
 });
 
 QUnit.test('Symbol#@@toStringTag', assert => {
